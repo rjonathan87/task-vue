db271a15576a29516ee750f0235da8b6455279ad
rjonathan87 <rjonathan87@gmail.com>
2019-10-07 15:30:17 -0500

    guardar en localStorage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,52 @@
 let app = new Vue({
     el: "#app",
     data: {
-        message: "mensaje"
+        task: "",
+        taskArr: []
+    },
+    methods: {
+        btnAddTask(e){
+            e.preventDefault();
+            let valueTask = e.target.form[0].value;
+            this.AddTask(valueTask);
+        },
+        AddTask(taskAdd, status = false){
+            this.taskArr.push({
+                "name": taskAdd,
+                "status": status
+            });
+            
+            //actualiza el localStorage
+            this.addDB();
+            document.getElementById("task").value = '';
+        },
+
+        // Agrega datos en el localStorage
+        addDB(){
+            localStorage.setItem('localDB', JSON.stringify(this.taskArr));
+        },
+
+        // Sirve para llenar el array después de una actualización,
+        // solo si el localStorage tiene datos
+        // Llena el array si el localStorage tiene datos
+        checkDB(){
+            if (localStorage.getItem('localDB') != null) {
+                // console.log("con datos");
+                let taskList = JSON.parse(localStorage.getItem('localDB'));
+                
+                taskList.forEach(element => {
+                    this.AddTask(element.name, element.status);
+                });
+            }else{
+                this.taskArr = [];
+            }
+        },
+        setFocus(){
+            document.getElementById("task").focus();
+        }
+    },
+    mounted() {
+        this.setFocus();
+        this.checkDB();
     }
 })
